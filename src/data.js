181a7ed2ed96dfa51b63fdc155e4d421166cfef5
milikkarty <milikkarty@gmail.com>
2022-07-27 14:40:06 +0300

    Add data file with menu and currency data

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const menuData = [
+    {
+        name: 'Cheeseburger',
+        price: 55.00,
+        img: 'https://www.kfc-ukraine.com/admin/files/medium/medium_4061.jpg'
+    },
+    {
+        name: 'Toast 2 Cheese',
+        price: 30.00,
+        img: 'https://www.kfc-ukraine.com/admin/files/medium/medium_3729.jpg'
+    },
+    {
+        name: 'Double Chicken',
+        price: 105.00,
+        img: 'https://www.kfc-ukraine.com/admin/files/medium/medium_4065.jpg'
+    },
+    {
+        name: 'Chickenburger',
+        price: 75.00,
+        img: 'https://www.kfc-ukraine.com/admin/files/medium/medium_4064.jpg'
+    },
+    {
+        name: 'Chickenburger Spicy',
+        price: 75.00,
+        img: 'https://www.kfc-ukraine.com/admin/files/medium/medium_4063.jpg'
+    },
+    {
+        name: 'Ukrainian Toast',
+        price: 45.00,
+        img: 'https://www.kfc-ukraine.com/admin/files/medium/medium_4068.jpg'
+    }
+];
+
+export const currencyData = ['UAH', 'USD', 'EUR'];
